a35515d5032e3cad5462c72d76b14410a18a78f2
Vladislav Urykov <vladislavurykov29@gmail.com>
2023-06-02 15:24:56 +0300

    import fix

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ProductI } from "@/app/page";
+import { ProductI } from "@/utils/stripe";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
